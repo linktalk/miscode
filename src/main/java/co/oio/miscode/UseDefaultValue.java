896f7417fd821cfcbfcd5d8765f2426af9a48f1e
LinkTalk <>
2019-05-11 08:31:44 -0700

    update filed names

--- src/main/java/co/oio/miscode/UseDefaultValue.java
+++ src/main/java/co/oio/miscode/UseDefaultValue.java
@@ -17,13 +17,13 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
  *       vs
  *       boolean v;    // use default value: false
  *
- * A sample of the Benchmark results (JDK 12):
+ * VolatileUseExplicitInitialization sample of the Benchmark results (JDK 12):
  * Benchmark                 Mode   Cnt           Score           Error  Units
- * withDefaultValue          thrpt   25  3713078690.377 ±  23089789.596  ops/s
- * noDefaultValue            thrpt   25  3701103280.943 ±  71561673.246  ops/s
+ * UseExplicitValue          thrpt   25  3713078690.377 ±  23089789.596  ops/s
+ * useDefaultValue           thrpt   25  3701103280.943 ±  71561673.246  ops/s
  *
- * withVolatileDefaultValue  thrpt   25  3587656113.778 ± 150725597.145  ops/s
- * noVolatileDefaultValue    thrpt   25  3715301582.315 ±  23250778.591  ops/s
+ * useExplicitVolatileValue  thrpt   25  3587656113.778 ± 150725597.145  ops/s
+ * useDefaultVolatileValue   thrpt   25  3715301582.315 ±  23250778.591  ops/s
  */
 public class UseDefaultValue {
     /**
@@ -51,7 +51,7 @@ public class UseDefaultValue {
     }
 
     /**
-     * A sample benchmark result (JDK 12):
+     * VolatileUseExplicitInitialization sample benchmark result (JDK 12):
      *   3587656113.778 ±(99.9%) 150725597.145 ops/s [Average]
      *   (min, avg, max) = (3064840103.958, 3587656113.778, 3738863833.856),
      *       stdev = 201214359.893
@@ -60,12 +60,12 @@ public class UseDefaultValue {
      */
     @Benchmark
     @Threads(Threads.MAX)
-    public void withVolatileDefaultValue() {
-        new A();
+    public void useExplicitVolatileValue() {
+        new VolatileUseExplicitInitialization();
     }
 
     /**
-     * A sample benchmark result (JDK 12):
+     * VolatileUseExplicitInitialization sample benchmark result (JDK 12):
      *   3715301582.315 ±(99.9%) 23250778.591 ops/s [Average]
      *   (min, avg, max) = (3613825116.698, 3715301582.315, 3743200186.669),
      *       stdev = 31039124.208
@@ -74,12 +74,12 @@ public class UseDefaultValue {
      */
     @Benchmark
     @Threads(Threads.MAX)
-    public void noVolatileDefaultValue() {
-        new B();
+    public void useDefaultVolatileValue() {
+        new VolatileUseDefaultInitialization();
     }
 
     /**
-     * A sample benchmark result (JDK 12):
+     * VolatileUseExplicitInitialization sample benchmark result (JDK 12):
      *   3713078690.377 ±(99.9%) 23089789.596 ops/s [Average]
      *   (min, avg, max) = (3625079740.417, 3713078690.377, 3739875971.640),
      *       stdev = 30824208.506
@@ -88,12 +88,12 @@ public class UseDefaultValue {
      */
     @Benchmark
     @Threads(Threads.MAX)
-    public void withDefaultValue() {
-        new C();
+    public void UseExplicitValue() {
+        new UseExplicitInitialization();
     }
 
     /**
-     * A sample benchmark result (JDK 12):
+     * VolatileUseExplicitInitialization sample benchmark result (JDK 12):
      *   3701103280.943 ±(99.9%) 71561673.246 ops/s [Average]
      *   (min, avg, max) = (3251156581.539, 3701103280.943, 3750684432.555),
      *       stdev = 95532786.387
@@ -102,14 +102,14 @@ public class UseDefaultValue {
      */
     @Benchmark
     @Threads(Threads.MAX)
-    public void noDefaultValue() {
-        new D();
+    public void useDefaultValue() {
+        new UseDefaultInitialization();
     }
 
     /**
-     * A class with assignment for a volatile field.
+     * Sample class with assignment for a volatile field.
      */
-    private class A {
+    private class VolatileUseExplicitInitialization {
         private volatile boolean a = false;
 
         private void set() {
@@ -118,9 +118,9 @@ public class UseDefaultValue {
     }
 
     /**
-     * A class without assignment for a volatile field.
+     * Sample class without assignment for a volatile field.
      */
-    private class B {
+    private class VolatileUseDefaultInitialization {
         private volatile boolean b;
 
         private void set() {
@@ -129,9 +129,9 @@ public class UseDefaultValue {
     }
 
     /**
-     * A class with assignment for a field.
+     * Sample class with assignment for a field.
      */
-    private class C {
+    private class UseExplicitInitialization {
         private boolean c = false;
 
         private void set() {
@@ -140,9 +140,9 @@ public class UseDefaultValue {
     }
 
     /**
-     * A class without assignment for a field.
+     * Sample class without assignment for a field.
      */
-    private class D {
+    private class UseDefaultInitialization {
         private boolean d;
 
         private void set() {
